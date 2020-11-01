ca45f844ca360209bb8615d4fad1997d42ced713
Minhee-Jeon <58028527+Minhee-Jeon@users.noreply.github.com>
2020-11-01 15:21:30 +0900

    [비구조화 할당 문법을 통해 props 내부 값 추출하기]
    MyComponent에서 props 값을 조회할 때마다 props.name, props.children 같이 props. 키워드를 앞에 붙여주고 있어요. 이 작업을 더 편하게 하기 위해 비구조화 할당 문법을 사용할게요.
    이 문법은 함수의 파라미터 부분에서도 사용 가능해요. 만약 함수의 파라미터가 객체라면 그 값을 바로 비구조화해서 사용하는 거예요.

--- 01.hello-react/src/MyComponent.js
+++ 01.hello-react/src/MyComponent.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const MyComponent = props => {
+// 비구조화 할당(destructuring assignment): 객체에서 값을 추출하는 문법. 함수의 파라미터 부분에서도 사용 가능하다.
+//const MyComponent = props => {
+//    const { name, children } = props;
+const MyComponent = ({ name, children }) => {
     return (
         <div>
-            안녕하세요, 제 이름은 {props.name}입니다. <br />
-            children 값은 {props.children}입니다.
+            안녕하세요, 제 이름은 {name}입니다. <br />
+            children 값은 {children}입니다.
             {/* 출력: 안녕하세요, 제 이름은 기본 이름입니다. \ children 값은 리액트입니다. */}
         </div>
     );
