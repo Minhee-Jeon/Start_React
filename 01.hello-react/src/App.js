31159397dff419fed3964243868b8ff3e1bcb9ae
Minhee-Jeon <58028527+Minhee-Jeon@users.noreply.github.com>
2020-11-01 13:33:07 +0900

    [undefined를 렌더링하지 않기]
    리액트 컴포넌트에서는 함수에서 undefined만 반환해 렌더링하는 상황을 만들면 안 돼요.'

--- 01.hello-react/src/App.js
+++ 01.hello-react/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'; 
+import './App.css';
 
 function App() { 
-  const name = '뤼ㅣㅣㅣ액트';
-  //return <div>{name === '리액트' ? <h1>리액트입니다.</h1> : null}</div>;
-  return <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>;
+  const name = undefined;
+  // return name;
+  // Error: App(...): Nothing was returned from render. This usually means a return statement is missing. Or, to render nothing, return null.
   
-  // Falsy한 값인 0은 예외적으로 화면에 나타난다. 
-  // const number = 0;
-  // return number && <div>내용</div> 
+  return name || '값이 undefined입니다.'; 
+  // 어떤 값이 undefined일 수도 있으면 이렇게 오류를 방자할 수 있어요.
+
+  return <div>name</div>;
+  // JSX 내부에서 undefined를 렌더링하는 것은 괜찮아요.
+
+  return <div>{name || '리액트'}</div>;
+  // name 값이 undefined일 때 보여주고 싶은 문구가 있으면 이렇게
 }
 
 export default App;
