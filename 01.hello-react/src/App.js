a3b1761a2fd6f3d9d7ddfb1eab7a865c0080fe15
Minhee-Jeon <58028527+Minhee-Jeon@users.noreply.github.com>
2020-11-01 13:25:49 +0900

    [&& 연산자를 사용한 조건부 렌더링]
    특정 조건을 만족할 때 내용을 보여주고, 만족하지 않을 때 아예 아무것도 렌더링하지 않을 때에도 조건부 연산자로 구현 가능해요. 5행 6행 모두 사용 가능한 코드예요.

--- 01.hello-react/src/App.js
+++ 01.hello-react/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react'; 
 
 function App() { 
-  const name = '리액트';
-  //const name = '뤼ㅣㅣㅣ액트';
-  return (
-    <div> 
-      {name === '리액트' ? (
-        <h1>리액트입니다.</h1>
-      ) : (
-        <h2>리액트가 아닙니다.</h2>
-      )}
-    </div>
-  );
+  const name = '뤼ㅣㅣㅣ액트';
+  //return <div>{name === '리액트' ? <h1>리액트입니다.</h1> : null}</div>;
+  return <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>;
+  
+  // Falsy한 값인 0은 예외적으로 화면에 나타난다. 
+  // const number = 0;
+  // return number && <div>내용</div> 
 }
 
 export default App;
