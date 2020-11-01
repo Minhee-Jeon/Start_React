4a6bc8a1cd65b89eafa05f3d7c43f4eee8863ced
Minhee-Jeon <58028527+Minhee-Jeon@users.noreply.github.com>
2020-11-01 14:28:51 +0900

    [첫 컴포넌트 생성]
    1. MyComponent.js (함수형 컴포넌트로 작성)
    2. App.js에서 MyComponent를 불러와서 렌더링해 사용해요.

--- 01.hello-react/src/App.js
+++ 01.hello-react/src/App.js
@@ -1,19 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
+import MyComponent from './MyComponent';
 
-class App extends Component() { 
-  render(){
-    const name = '리액트';
-    return (
-      <>
-        {/* 주석은 이렇게 작성해요. */}
-        <div className="react" // 시작 태그를 여러 줄로 작성하게 되면 여기에 주석을 작성할 수 있어요.
-        >
-          {name}
-        </div>
-        <input />
-      </>
-    );
-  }
+const App = () => {
+  return <MyComponent />;
 }
 
 export default App;
