5625538a1da6417272b021d6d08d4fe821e3280a
Minhee-Jeon <58028527+Minhee-Jeon@users.noreply.github.com>
2020-11-01 13:11:27 +0900

    [JavaScript 표현식 쓰기]
    JSX 안에서는 자바스크립트 표현식을 쓸 수 있어요.
    자바스크립트 표현식을 쓰려면 JSX 내부에서 코드를 {}로 감싸면 돼요.

--- 01.hello-react/src/App.js
+++ 01.hello-react/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'; 
 
 function App() { 
-  //<div>나 <Fragment>나 <>로 감싸줘야 한다!
-  // Virtual DOM에서 컴포넌트 변화를 감지할 때 효율성을 위해 컴포넌트 내부는 하나의 DOM트리 구조로 이뤄져야 한다는 규칙이 있다.
-  // Parsing error: Adjacent JSX elements must be wrapped in an enclosing tag. Did you want a JSX fragment <>...</>?
+  const name = '리액트';
   return (
     <> 
-      <h1>리액트 안녕!</h1>
+      <h1>{name} 안녕!</h1>
       <h2>잘 작동하니?</h2>
     </>
   );
