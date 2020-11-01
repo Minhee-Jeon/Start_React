fdf602cbfd78c66aa2c0be5379f0690e3fb2976b
Minhee-Jeon <58028527+Minhee-Jeon@users.noreply.github.com>
2020-11-01 14:39:42 +0900

    [props 기본값 설정: defaultProps]
    props 값을 따로 지정하지 않았을 때 보여줄 기본값을 설정하는 defaultProps

--- 01.hello-react/src/App.js
+++ 01.hello-react/src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent name="React" />; //출력: 안녕하세요, 제 이름은 React입니다.
-  //return <MyComponent />;   //출력: 안녕하세요, 제 이름은 입니다.
+  return <MyComponent />; //출력: 안녕하세요, 제 이름은 기본 이름입니다.
 }
 
 export default App;
