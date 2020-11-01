fc652e1e165305f64f8b03aa054fed68b5c7837d
Minhee-Jeon <58028527+Minhee-Jeon@users.noreply.github.com>
2020-11-01 13:45:50 +0900

    [class 대신 className]
    일반 HTML에서 CSS 클래스 사용 시에는 <div class="클래스 이름"></div>처럼 class 속성을 이용했어요. 하지만 JSX에서는 class가 아니라 className으로 설정해줘야 해요.

--- 01.hello-react/src/App.js
+++ 01.hello-react/src/App.js
@@ -1,18 +1,9 @@
 import React from 'react'; 
+import './App.css';
 
 function App() { 
   const name = '리액트';
-  const style = {
-    //Camel Case로 표기
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16 //단위 생략 시 px로 지정돼요.
-  };
-  return <div style={style}>{name}</div>;
-
-  //미리 style객체를 선언하지 않고 바로 값을 지정하고 싶다면 {}; 안의 내용을 {style}에 위치시켜요.
+  return <div className="react">{name}</div>;
 }
 
 export default App;
