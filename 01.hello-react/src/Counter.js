18adcabfb388db06b3659ca8262dca17938a29f4
전민희 <skdixm@synapsoft.co.kr>
2020-11-05 10:48:00 +0900

    [state를 constructor에서 꺼내기 & this.setState에 객체 대신 함수 인자 전달하기]
    this.setState()로 state값을 업데이트할 때엔 상태가 비동기적으로 업데이트돼요.
    this.setState를 사용한다고 state값이 바로 바뀌지는 않아서 주석 부분과같이 this.setState()를 두 번 호출해도 숫자는 1씩 더해져요.
    
    this.setState()사용 시 객체 대신 함수를 인자로 넣어주면 이를 해결할 수 있어요. 객체 대신 return { //업데이트할 내용 }

--- 01.hello-react/src/Counter.js
+++ 01.hello-react/src/Counter.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 
 class Counter extends Component {
-    constructor(props) { // 컴포넌트의 생성자 메서드: 컴포넌트에 state를 설정
-        super(props); // 필수! 현재 클래스형 컴포넌트가 상속하고 있는 리액트의 Component 클래스가 지닌 생성자 함수를 호출
-        
-        this.state = { // state의 초깃값 설정하기. 컴포넌트의 state는 객체 형식이어야 한다.
-            number: 0,
-            fixedNumber: 0
-        };
-    }
+    //이렇게 하면 constructor() 선언 없이 state 초깃값을 설정할 수 있어요.
+    state = {
+        number: 0,
+        fixedNumber: 0
+    };
     render() {
-        const { number } = this.state; // state를 조회할 때에는 this.state로
+        const { number, fixedNumber } = this.state; // state를 조회할 때에는 this.state로
         return (
             <div>
                 <h1>{number}</h1>
                 <h2>바뀌지 않는 값: {fixedNumber}</h2>
-                <button
-                    // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정해요.
-                    onClick={() => {
-                    // this.setState를 사용해 state에 새로운 값을 넣을 수 있어요.
-                    this.setState({ number: number + 1 });
+                <button onClick={() => {
+                    // this.setState({ number: number + 1 });
+                    // this.setState({ number: this.state.number + 1}); // this.setState를 두번 사용해도 버튼 클릭마다 숫자가 1씩만 더해져요.
+                        this.setState(prevState => { // 화살표 함수에서 값을 반환하고 싶으면 { }를 생략하면 돼요.
+                            return {
+                                number: prevState.number + 1
+                            };
+                        });
+                        // 위 코드와 아래 코드는 완전히 똑같은 기능을 해요. 함수에서 바로 객체를 반환한다는 의미예요.
+                        this.setState(prevState => ({
+                                number: prevState.number + 1
+                            }));
                     }}
                 >
                     +1
