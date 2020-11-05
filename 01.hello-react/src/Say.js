66c0e1cfc27504b2334b8225cc11f91be3377351
전민희 <skdixm@synapsoft.co.kr>
2020-11-05 11:08:21 +0900

    [함수형 컴포넌트에서 useState 사용하기]
    리액트 16.8 이후부터는 함수형 컴포넌트에서도 useState()로 state를 사용할 수 있게 되었어요.
    이 과정에서 다양한 종류의 Hooks를 사용할 수 있지만, 여기선 useState만 써요.

---
+++ 01.hello-react/src/Say.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const Say = () => {
+    // useState() 인자에는 상태의 초깃값이 들어가는데, 클래스형 컴포넌트처럼 꼭 객체 형태를 넣지 않아도 된다. (형태: 자유)
+    const [message, setMessage] = useState('');
+    const onClickEnter = () => setMessage('안녕하세요!');
+    const onClickLeave = () => setMessage('안녕히 가세요!');
+
+    const [color, setColor] = useState('black');
+
+    return (
+        <div>
+            <button onClick={onClickEnter}>입장</button>
+            <button onClick={onClickLeave}>퇴장</button>
+            <h1 style={{ color }}>{message}</h1>
+
+            <button style={{ color: 'red'}} onClick={() => setColor('red')}>
+                빨간색
+            </button>
+            <button style={{ color: 'green'}} onClick={() => setColor('green')}>
+                초록색
+            </button>
+            <button style={{ color: 'blue'}} onClick={() => setColor('blue')}>
+                파란색
+            </button>
+        </div>
+    );
+};
+
+export default Say;
